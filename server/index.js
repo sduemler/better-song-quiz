0f6dd7928c6be5202974995e514d204535f05a12
sduemler <samduemler@gmail.com>
2024-02-29 17:27:19 -0500

    Basic playback functionality

--- server/index.js
+++ server/index.js
@@ -4,28 +4,37 @@ const dotenv = require('dotenv');
 
 const port = 5001;
 
-global.access_token = '';
+var access_token = '';
 
 dotenv.config();
 
 var spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
 var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
+var spotify_redirect_uri = 'http://localhost:3000/auth/callback';
+
+var generateRandomString = function (length) {
+  var text = '';
+  var possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
 var app = express();
 
 app.get('/auth/login', (req, res) => {
-  var scope =
-    'streaming \
-               user-read-email \
-               user-read-private';
-
+  var scope = 'streaming user-read-email user-read-private';
   var state = generateRandomString(16);
 
   var auth_query_parameters = new URLSearchParams({
     response_type: 'code',
     client_id: spotify_client_id,
     scope: scope,
-    redirect_uri: 'http://localhost:3000/auth/callback',
+    redirect_uri: spotify_redirect_uri,
     state: state,
   });
 
@@ -42,7 +51,7 @@ app.get('/auth/callback', (req, res) => {
     url: 'https://accounts.spotify.com/api/token',
     form: {
       code: code,
-      redirect_uri: 'http://localhost:3000/auth/callback',
+      redirect_uri: spotify_redirect_uri,
       grant_type: 'authorization_code',
     },
     headers: {
@@ -58,29 +67,16 @@ app.get('/auth/callback', (req, res) => {
 
   request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
+      access_token = body.access_token;
       res.redirect('/');
     }
   });
 });
 
 app.get('/auth/token', (req, res) => {
-  res.json({
-    access_token: access_token,
-  });
+  res.json({ access_token: access_token });
 });
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
-
-var generateRandomString = function (length) {
-  var text = '';
-  var possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
